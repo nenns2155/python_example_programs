4db594ca8debcdf4ff2f051ea58de74982ed5fb1
Nathan E <nenns@ksu.edu>
2021-09-22 14:13:36 -0500

    Added first_largest

--- first_largest.py
+++ first_largest.py
@@ -2,14 +2,22 @@
 Python program to find the largest element and its location.
 """
 
-def largest_element(a):
+def largest_element(a,Flag):
     """ Return the largest element of a sequence a.
     """
-
-    return None
+    max = a[0]
+    loc = 0
+    for i in range(1,len(a)):
+        if a[i] > max:
+            max=a[i]
+            loc = i
+            
+    if Flag == True:
+        return max, loc
+    return max
 
 
 if __name__ == "__main__":
 
-    a = [1,2,3,2,1]
+    a = [1,2,5,2,1]
     print("Largest element is {:}".format(largest_element(a)))
--- tests.py
+++ tests.py
@@ -7,10 +7,14 @@ class FirstLargest(unittest.TestCase):
     def test_largest_is_first(self):
         given = [1,2,3,2,1]
         expect = 3
-        got = largest_element(given)
+        got = largest_element(given,False)
         self.assertEqual(got, expect)
    
-    """ ADD MORE  """
+    def test_largest_is_first_location(self):
+        given = [1,2,3,2,1]
+        expect = 3, 2
+        got = largest_element(given, True)
+        self.assertEqual(got, expect)
 
 if __name__ == '__main__':
     unittest.main()
